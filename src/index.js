4cdcc19cc876f8c2a18167f05813d26ee6e1f4ff
Bobby Singh <b830103@gmail.com>
2023-12-16 22:24:27 +0100

    Added atatus-spa

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
+import * as atatus from "atatus-spa";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
