616632c4fdd2d51c389f3f45e324b7a79078941a
Bobby Singh <b830103@gmail.com>
2023-12-21 22:56:24 +0100

    Implementing a Scatterplot with Recharts

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
 import "./App.css";
+import CityEventsChart from "./components/CityEventsChart";
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -53,6 +54,9 @@ const App = () => {
         setCurrentNOE={setCurrentNOE}
         setErrorAlert={setErrorAlert}
       />
+      <div className="charts-container">
+        <CityEventsChart allLocations={allLocations} events={events} />
+      </div>
       <EventList events={events} />
     </div>
   );
