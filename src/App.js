6b3589bfd357ed813f3da651ebde5e412285b4e4
Bobby Singh <b830103@gmail.com>
2023-12-22 22:58:44 +0100

    Implement a Pie Chart

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import NumberOfEvents from "./components/NumberOfEvents";
 import { useEffect, useState } from "react";
 import { extractLocations, getEvents } from "./api";
 import { InfoAlert, ErrorAlert, WarningAlert } from "./components/Alert";
-import "./App.css";
 import CityEventsChart from "./components/CityEventsChart";
+import EventGenresChart from "./components/EventGenresChart";
+import "./App.css";
 
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
@@ -56,6 +57,7 @@ const App = () => {
       />
       <div className="charts-container">
         <CityEventsChart allLocations={allLocations} events={events} />
+        <EventGenresChart events={events} />
       </div>
       <EventList events={events} />
     </div>
---
+++ src/components/EventGenresChart.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { ResponsiveContainer, PieChart, Pie, Legend } from "recharts";
+
+const EventGenresChart = ({ events }) => {
+  const [data, setData] = useState([]);
+  const genres = ["React", "JavaScript", "Node", "jQuery", "AngularJS"];
+
+  useEffect(() => {
+    setData(getData());
+  }, [`${events}`]);
+
+  const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    outerRadius,
+    percent,
+    index,
+  }) => {
+    const RADIAN = Math.PI / 180;
+    const radius = outerRadius;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
+    const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
+    return percent ? (
+      <text
+        x={x}
+        y={y}
+        fill="#8884d8"
+        textAnchor={x > cx ? "start" : "end"}
+        dominantBaseline="central"
+      >
+        {`${genres[index]} ${(percent * 100).toFixed(0)}%`}
+      </text>
+    ) : null;
+  };
+
+  // Declares the getData function using arrow function syntax
+  const getData = () => {
+    // Maps over the 'genres' array to transform each genre into an object
+    const data = genres.map((genre) => {
+      // Filters the 'events' array to count events where the summary includes the current genre
+      const filteredEvents = events.filter((event) =>
+        event.summary.includes(genre)
+      ).length;
+      // Returns an object for each genre with its name and the count of related events
+      return { name: genre, value: filteredEvents };
+    });
+
+    // Returns the transformed array of objects, each representing a genre and its event count
+    return data;
+  };
+
+  // 'getData'  called to get the array of genres with their associated event counts
+
+  // Renders the chart using the 'data' array returned by 'getData'
+  return (
+    <ResponsiveContainer width="99%" height={400}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="value"
+          fill="#8884d8"
+          label={renderCustomizedLabel}
+          labelLine={false}
+          outerRadius={130}
+        />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default EventGenresChart;
